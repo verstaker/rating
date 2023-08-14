d34f56a5bbc8494b6581e5a49b721b92bfed59c2
verstaker <90459525+verstaker@users.noreply.github.com>
2023-08-14 16:36:43 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $('.contests__class').on('click',function(){
-	var tab = $(this).attr('tab-class');
+	var tab = $(this).attr('data-tab');
 	if (!$(this).hasClass('contests__class_active')) {
 		$(this).parent().children('.contests__class').removeClass('contests__class_active');
 		$(this).addClass('contests__class_active');
-		$(this).parents('.main-content__inner').find('.contests__tab').hide(0).removeClass('contests__tab_active');
+		$(this).parents('.offer__inner').find('.contests__tab').hide(0).removeClass('contests__tab_active');
 		$('#'+tab).show(0).addClass('contests__tab_active');
 	}
 })
